307c55d392e232596930c0265323b58564a263dd
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-04-09 22:02:39 -0400

    button

--- src/mainmenu.py
+++ src/mainmenu.py
@@ -3,12 +3,16 @@ from tkinter.font import Font
 from PIL import ImageTk, Image
 
 
+
+
+
 class MainMenu(Frame):
     
     def __init__(self, parent, controller, width, height):
         Frame.__init__(self, parent, width=width, height=height)
         #self.statusLabel = tkinter.Label(parent, text="", width= 20)
         #self.statusLabel.grid()
+        self.parent = parent
 
         
 
@@ -28,6 +32,14 @@ class MainMenu(Frame):
         slant="roman",
 	underline=0,
 	overstrike=0)
+
+        buttonFont = Font(
+	family="Georgia",
+	size=40,
+        slant="roman",
+	underline=0,
+	overstrike=0)
+        
 
         #displays title of the game
         self.title = Label(self, text="Welcome To", width=20,bg="pink", font= mainFont_i)
@@ -49,5 +61,15 @@ class MainMenu(Frame):
         self.title.pack(pady=10)
         self.title2.pack(pady=10)
         self.imglabel.pack()
-        
+
+
+        #links
+       # button1= Button(self, text="About", width=20, bg="pink",font= buttonFont, command=self.parent.master.showAbout())
+       # button1.pack(pady=10)
+
+        button2= Button(self, text="Play", width=20, bg="pink",font= buttonFont, command=self.parent.master.showGame())
+        button2.pack(pady=10)
+
+        #button2= Button(self, text="View High Scores", width=20, bg="pink",font= buttonFont, command= self.parent.master.showScore())
+       # button2.pack(pady=10)
         self.controller = controller
