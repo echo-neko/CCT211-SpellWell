d48a0e365e2d5ed4adf343c5159d3b809f6b8938
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-04-10 16:36:35 -0400

    Update about.py

--- src/about.py
+++ src/about.py
@@ -12,7 +12,7 @@ class About(Frame):
         # Define Fonts
         textFont = Font(
 	family="Georgia",
-	size=16,
+	size=15,
         slant="roman",
 	underline=0,
 	overstrike=0)
@@ -34,21 +34,19 @@ class About(Frame):
         self.about_title = Label(self,text="About", bg="pink", font=titleFont)
         self.about_title.pack(pady=10)
 
-        self.about_desc = Label(self, text="Spell Well is a game for learning new words along \n with their definitions. We want to help out all kinds \n of people learning new words or even students \n who need to memorize complicated vocabulary \n for their courses by providing them with a fun way \n of practicing their spelling",  bg="pink", font=textFont)
-        self.about_desc.pack(pady=5, padx=10)
+        self.about_desc = Label(self, text="Spell Well is a game for learning new words along with their definitions. We want to help out all kinds of people learning new words or even students who need to memorize complicated vocabulary for their courses by providing them with a fun way of practicing their spelling", wraplength=450, bg="pink", font=textFont)
+        self.about_desc.pack()
 
         self.how_title = Label(self,text="How to Play", bg="pink", font=titleFont)
-        self.how_title.pack(pady=10)
+        self.how_title.pack(pady=8)
 
-        self.how_desc = Label(self,text="The game is played by answering words \n that match with the definition shown on screen. \n The player’s goal is to guess all the right \n  words in the time provided!",bg="pink", font=textFont)
-        self.how_desc.pack(pady=5, padx=10)
+        self.how_desc = Label(self,text="The game is played by answering words that match with the definition shown on screen. The player’s goal is to guess all the right words in the time provided!", wraplength=450, bg="pink", font=textFont)
+        self.how_desc.pack()
 
-        self.preset_desc = Label(self,text="We provide a few preset dictionaries to play with, \n but players have the ability to create unique \n dictionaries with their own terms and definitions as well.",bg="pink", font=textFont)
-        self.preset_desc.pack(pady=5, padx=10)
+        self.preset_desc = Label(self,text="We provide a few preset dictionaries to play with, but players have the ability to create unique dictionaries with their own terms and definitions as well. The score will be calculated based on the time spent on each word.", wraplength=450, justify="center", bg="pink", font=textFont)
+        self.preset_desc.pack()
 
-        self.score_desc = Label(self,text="The score will be calculated based on the time spent on each word." , bg="pink", font=textFont)
-        self.preset_desc.pack(pady=5, padx=10)
   
-        button2= Button(self, text="Play", height=2, width=8,highlightbackground='pink', fg="black", font= buttonFont, command=self.parent.master.showDictList)
-        button2.pack(padx=10)
+        button2= Button(self, text="Play", height=2, width=8, highlightbackground='pink', bg='pink', fg="black", font= buttonFont, command=self.parent.master.showDictList)
+        button2.pack(pady=3)
 
