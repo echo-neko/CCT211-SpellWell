d6ccf09f50d9909ea7dc503bb2fbf01646af4fe9
jasxaxp <70676153+jasxaxp@users.noreply.github.com>
2022-04-10 20:39:36 -0400

    Update editdict.py

--- src/editdict.py
+++ src/editdict.py
@@ -13,7 +13,7 @@ class EditDict(Frame):
 
         valCommand = (self.register(self.isText), '%S')
 
-        self.nameEntry = Entry(self, validate="all", validatecommand=valCommand)
+        self.nameEntry = Entry(self, validate="all", validatecommand=valCommand, font=("Garamond", 16))
         self.nameEntry.pack()
 
         self.table_frame = Frame(self)
@@ -21,23 +21,23 @@ class EditDict(Frame):
         
         self.currIID = 0
         
-        self.definition = Label(self, text="", bg='pink')
-        self.definition.pack()
+        self.definition = Label(self, text="", bg='pink', font=("Garamond", 16))
+        self.definition.pack(pady=2)
 
         self.wordEntry = Entry(self, validate="all", validatecommand=valCommand)
-        self.wordEntry.pack()
+        self.wordEntry.pack(pady=2)
 
-        self.defEntry = Text(self, width = 50 , bg='pink' )
-        self.defEntry.pack()
+        self.defEntry = Text(self, width = 40, bg='#cca0bb', font=("Garamond", 16))
+        self.defEntry.pack(padx=4)
         
-        self.wordButton = Button(self, text="Add", bg='pink' ,width=10, command=self.addEntry)
-        self.wordButton.pack()
+        self.wordButton = Button(self, text="Add" ,width=10, command=self.addEntry,highlightbackground='#c17b9f', bg='#c17b9f', fg="black", font=("Georgia", 15))
+        self.wordButton.pack(pady=5)
 
-        self.saveButton = Button(self, text="Save and Return to List", command=self.saveReturn,  highlightbackground='#cca0bb', bg='#cca0bb', fg="black", font=("Georgia", 16))
-        self.saveButton.pack()
+        self.saveButton = Button(self, text="Save and Return to List", command=self.saveReturn, width=30,highlightbackground='#cca0bb', bg='#cca0bb', fg="black", font=("Georgia", 15))
+        self.saveButton.pack(pady=5)
 
-        self.noSaveButton = Button(self, text="Return to List without Saving",command=self.noSaveReturn, highlightbackground='#cca0bb', bg='#cca0bb', fg="black", font=("Georgia", 16))
-        self.noSaveButton.pack()
+        self.noSaveButton = Button(self, text="Return to List without Saving",command=self.noSaveReturn,width=30, highlightbackground='#cca0bb', bg='#cca0bb', fg="black", font=("Georgia", 15))
+        self.noSaveButton.pack(pady=5)
 
         self.statusLabel = Label(self, text="", width=20, highlightbackground='pink', bg='pink', fg="black", font=("Georgia", 16))
         self.statusLabel.pack()
@@ -45,11 +45,11 @@ class EditDict(Frame):
 
     def addEntry(self):
         if self.wordEntry.get() == "":
-            self.statusLabel.configure(text="Word cannot be empty")
+            self.statusLabel.configure(text="Word cannot be empty", bg='pink', font=("Garamond", 18))
             return
 
         if self.defEntry.get("1.0",'end-1c') == "":
-            self.statusLabel.configure(text="Definition cannot be empty")
+            self.statusLabel.configure(text="Definition cannot be empty",bg='pink', font=("Garamond", 18))
             return
 
         if self.isText(self.wordEntry.get()):
@@ -61,7 +61,7 @@ class EditDict(Frame):
                 self.setText("", "")
                 self.statusLabel.configure(text="")
         else:
-            self.statusLabel.configure(text="Invalid characters in word")
+            self.statusLabel.configure(text="Invalid characters in word", bg='pink', font=("Garamond", 18))
 
 
     def isText(self, text):
@@ -99,7 +99,15 @@ class EditDict(Frame):
         self.dictTable.heading("word",text="Word",anchor=CENTER)
         self.dictTable.heading("definition",text="Definition",anchor=CENTER)
         
-        self.dictTable.pack()
+        # source: https://www.pythontutorial.net/tkinter/tkinter-theme/
+        style = ttk.Style(self)
+        # set ttk theme to "clam" which support the fieldbackground option
+        style.theme_use("clam")
+        style.configure("Treeview", background="#c17b9f", 
+                        fieldbackground="#c17b9f", foreground="white",font=("Georgia", 15))
+
+        self.dictTable.pack(padx=4)
+
 
         self.currIID = 0
 
@@ -120,11 +128,11 @@ class EditDict(Frame):
 
     def saveReturn(self):
         if self.nameEntry.get() == "":
-            self.statusLabel.configure(text="Dictionary name cannot be empty")
+            self.statusLabel.configure(text="Dictionary name cannot be empty", bg='pink', font=("Garamond", 18))
             return
 
         if not self.isText(self.nameEntry.get()):
-            self.statusLabel.configure(text="Invalid characters in dictionary name")
+            self.statusLabel.configure(text="Invalid characters in dictionary name",bg='pink', font=("Garamond", 18))
             return
 
         if const.CURRDICT != self.nameEntry.get():
@@ -135,7 +143,7 @@ class EditDict(Frame):
                 const.Db.deleteDict(const.CURRDICT)
                 const.CURRDICT = self.nameEntry.get()
             else: 
-                self.statusLabel.configure(text="Dictionary name already exists")
+                self.statusLabel.configure(text="Dictionary name already exists", bg='pink', font=("Garamond", 18))
         const.Db.saveDict(const.CURRDICT, self.dict)
         self.noSaveReturn()
 
